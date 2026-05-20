3d03886a6488df3de6c5f9d3916d3c5ddd08c0bb
Nelson Torres <nelsontorresjr330@gmail.com>
2026-05-20 09:58:46 -0500

    bugfix

--- src/pages/LaplacianGLMPage.jsx
+++ src/pages/LaplacianGLMPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid,
-  Tooltip, Legend, ReferenceLine, ResponsiveContainer,
+  Tooltip, ReferenceLine, ResponsiveContainer,
 } from 'recharts';
 
 // Set REACT_APP_GLM_API_URL in a .env file after CloudFormation deploys.
